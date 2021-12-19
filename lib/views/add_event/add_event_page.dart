97a750c31acf97615f7654bc613236d640afc022
Harinandan <harinandan.alukkal@gmail.com>
2021-12-19 12:52:53 +0530

    Saving events in memory

--- lib/views/add_event/add_event_page.dart
+++ lib/views/add_event/add_event_page.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_101_todo/views/add_event/widgets/save_event_button.dart';
 
-class AddEvent extends StatelessWidget {
-  const AddEvent({ Key? key }) : super(key: key);
+class AddEvent extends StatefulWidget {
+  const AddEvent({Key? key}) : super(key: key);
+
+  @override
+  State<AddEvent> createState() => _AddEventState();
+}
+
+class _AddEventState extends State<AddEvent> {
+  String eventName = "";
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Text('Hello'),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 10,
+            right: 10,
+            top: 100,
+            bottom: 30,
+          ),
+          child: Column(
+            children: [
+              const Text(
+                'CREATE NEW TODO',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                child: TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter the name of event/activity',
+                    ),
+                    onChanged: (val){
+                      setState(() {
+                        eventName = val;
+                      });
+                      print(eventName);
+                    },
+                  ),
+              ),
+            ]
+          ),
         ),
-        floatingActionButton: SaveEventFAB(),
+      ),
+      floatingActionButton: SaveEventFAB(
+        eventLabel: eventName 
       ),
     );
   }
--- lib/views/add_event/widgets/save_event_button.dart
+++ lib/views/add_event/widgets/save_event_button.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_101_todo/controllers/appdata.dart';
+import 'package:flutter_101_todo/models/event.dart';
 
 class SaveEventFAB extends StatelessWidget {
-  const SaveEventFAB({Key? key}) : super(key: key);
+  final String eventLabel;
+
+  const SaveEventFAB({
+    Key? key,
+    required this.eventLabel,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    print(eventLabel);
     return FloatingActionButton(
       backgroundColor: Colors.black,
       onPressed: () {
-        Navigator.of(context).pop();
+        print(eventLabel);
+        if(eventLabel.isNotEmpty){
+          globalEventList.add(Event(data: eventLabel));
+          Navigator.of(context).pop();
+          print(globalEventList);
+        }
       },
       child: const Icon(
         Icons.check,
