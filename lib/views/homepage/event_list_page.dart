f9a060ae956957d6dbc0986e84c69c20ddb55e78
Harinandan <harinandan.alukkal@gmail.com>
2021-12-19 11:10:44 +0530

    HomePage: Basic Layout

--- lib/views/homepage/event_list_page.dart
+++ lib/views/homepage/event_list_page.dart
@@ -5,9 +5,37 @@ class EventListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello, World'),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10,),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                'TODO LIST',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      child: Text('Event 1'),
+                    ),
+                    Container(
+                      child: Text('Event 2'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
